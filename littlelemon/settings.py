236a1b9508898e530ae1a0f139243a306af26770
amankhan <aman@littlelemon.com>
2023-11-18 20:22:16 +0530

    ready to deploy

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # The settings for static files have been updated for the Graded assessment
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
 ]
 
 # Default primary key field type
