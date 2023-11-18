fc96b159037dd5a72c1581ff26ffee136e122207
amankhan <aman@littlelemon.com>
2023-11-18 18:40:27 +0530

    updated settings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The settings for static files have been updated for the Graded assessment
-STATIC_URL = 'restaurant/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "restaurant/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
