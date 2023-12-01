89cd61ef1f01a45821ea5a1cf3877910345e83ef
amankhan <aman@littlelemon.com>
2023-12-01 16:57:03 +0530

    settings changed

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'bq0swyf0w1%qwbf8s885993cn_^&u=h@26_ry2z@zpzuje$86w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
